3a9f16a8fc3be695a84749db06d25e3c1525d93d
raviitsme <its.ravimohan01@gmail.com>
2026-07-27 20:04:18 +0530

    NinjaHub Sync: Solved Unbounded Knapsack

--- Moderate/Unbounded-Knapsack/Solution.cpp
+++ Moderate/Unbounded-Knapsack/Solution.cpp
@@ -1,23 +1,23 @@
-int f(int ind, int W, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp) {
-    if(ind == 0) {
-        return (W/wt[0]) * val[0];
-    }
- 
-    if(dp[ind][W] != -1) {
-        return dp[ind][W];
-    }
- 
-    int notTake = 0 + f(ind - 1, W, val, wt, dp);
-    int take = -1e9;
-    if(wt[ind] <= W) {
-        take = val[ind] + f(ind, W - wt[ind], val, wt, dp);
-    } 
- 
-    return dp[ind][W] = max(take, notTake);
+int f(int ind, int W, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp) {
+    if(ind == 0) {
+        return (W/wt[0]) * val[0];
+    }
+​
+    if(dp[ind][W] != -1) {
+        return dp[ind][W];
+    }
+​
+    int notTake = 0 + f(ind - 1, W, val, wt, dp);
+    int take = -1e9;
+    if(wt[ind] <= W) {
+        take = val[ind] + f(ind, W - wt[ind], val, wt, dp);
+    } 
+​
+    return dp[ind][W] = max(take, notTake);
 }
- 
-int unboundedKnapsack(int n, int w, vector<int> &val, vector<int> &wt){
-    // Write Your Code Here.
-    vector<vector<int>> dp(n, vector<int>(w + 1, -1));
-    return f(n - 1, w, val, wt, dp);
+​
+int unboundedKnapsack(int n, int w, vector<int> &val, vector<int> &wt){
+    // Write Your Code Here.
+    vector<vector<int>> dp(n, vector<int>(w + 1, -1));
+    return f(n - 1, w, val, wt, dp);
 }
