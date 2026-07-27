c73dd33426c70fe883194fcfc00810376fe42557
raviitsme <its.ravimohan01@gmail.com>
2026-07-27 20:10:24 +0530

    NinjaHub Sync: Solved Unbounded Knapsack

--- Moderate/Unbounded-Knapsack/Solution.cpp
+++ Moderate/Unbounded-Knapsack/Solution.cpp
@@ -1,16 +1,3 @@
-int f(int ind, int W, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp) {
-    if(ind == 0) {
-        return (W/wt[0]) * val[0];
-    }
- 
-    if(dp[ind][W] != -1) {
-        return dp[ind][W];
-    }
- 
-    int notTake = 0 + f(ind - 1, W, val, wt, dp);
-    int take = -1e9;
-    if(wt[ind] <= W) {
-        take = val[ind] + f(ind, W - wt[ind], val, wt, dp);
     } 
  
     return dp[ind][W] = max(take, notTake);
@@ -24,3 +11,16 @@ int unboundedKnapsack(int n, int w, vector<int> &val, vector<int> &wt){
     }
  
     for(int ind = 1; ind < n; ind++) {
+        for(int W = 0; W <= w; W++) {
+            int notTake = 0 + dp[ind - 1][W];
+            int take = -1e9;
+            if(wt[ind] <= W) {
+                take = val[ind] + dp[ind][W - wt[ind]];
+            } 
+ 
+            dp[ind][W] = max(take, notTake);
+        }
+    }
+ 
+    return dp[n - 1][w];
+}
