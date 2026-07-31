692ec0f7575380aa00c21b01b0afef58a1b859f4
raviitsme <its.ravimohan01@gmail.com>
2026-07-31 13:40:52 +0530

    NinjaHub Sync: Solved Longest Common Substring (Solution_2.cpp)

---
+++ Moderate/Longest-Common-Substring/Solution_2.cpp
@@ -0,0 +1,35 @@
+int f(string &s, string &t)
+{
+	//Write your code here
+	int m = s.size();
+	int n = t.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+	for(int j = 0; j <= n; j++) {
+		dp[0][j] = 0;
+	}
+
+	for(int i = 0; i <= m; i++) {
+		dp[i][0] = 0;
+	}
+
+    int ans = 0;
+
+	for(int i = 1; i <= m; i++) {
+		for(int j = 1; j <= n; j++) {
+			if(s[i - 1] == t[j - 1]) {
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+                ans = max(ans, dp[i][j]);
+            } else {
+				dp[i][j] = 0;
+			}
+		}
+	}
+
+	return ans;
+}
+
+int lcs(string &str1, string &str2){
+    // Write your code here.
+    return f(str1, str2);
+}
