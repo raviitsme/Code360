46a337fcf85bd102d504cd025944568f07d53b0d
raviitsme <its.ravimohan01@gmail.com>
2026-08-06 18:51:56 +0530

    NinjaHub Sync: Solved Minimum Number of Deletions and Insertions (Solution.cpp)

---
+++ Easy/Minimum-Number-of-Deletions-and-Insertions/Solution.cpp
@@ -0,0 +1,29 @@
+int lcs(string s, string t)
+{
+	//Write your code here
+	int m = s.size();
+	int n = t.size();
+	vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+	for(int j = 0; j <= n; j++) {
+		dp[0][j] = 0;
+	}
+
+	for(int i = 0; i <= m; i++) {
+		dp[i][0] = 0;
+	}
+
+	for(int i = 1; i <= m; i++) {
+		for(int j = 1; j <= n; j++) {
+			if(s[i - 1] == t[j - 1]) {
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			} else {
+				dp[i][j] = max((dp[i - 1][j]), (dp[i][j - 1]));
+			}
+		}
+	}
+
+	return dp[m][n];
+}int canYouMake(string &s1, string &s2){
+    return s1.size() + s2.size() - 2*lcs(s1, s2);
+    // Write your code here.
+}
